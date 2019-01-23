ceb194aa02467da968e5d74def15b4f07bcc18c6
moeinrahimi <imoeinrahimi1@gmail.com>
2019-01-23 16:58:07 +0330

    Updates

---
+++ precache-manifest.38e9b62a8dcfa7857a241132fccf4c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f20fc09347c0e9eecc8",
+    "url": "/static/js/main.1f20fc09.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1f20fc09347c0e9eecc8",
+    "url": "/static/css/main.ff994cdc.chunk.css"
+  },
+  {
+    "revision": "36a84670a1c37e102ad32f98e39deb74",
+    "url": "/index.html"
+  }
+];
